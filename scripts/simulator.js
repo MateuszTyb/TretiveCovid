62f011e78d81b310fcdc03d1818e2e1f71f0ef9a
unknown <tybinkowski.mateusz@gmail.com>
2022-10-25 23:39:52 +0200

    Funkcje

--- scripts/simulator.js
+++ scripts/simulator.js
@@ -1,16 +1,55 @@
-const population = [{
+let parameters = {
+    simulationNameVar: 0,
+    populationSizeVar: 0,
+    firstInfectedVar: 0,
+    rFactorVar: 0,
+    mFactorVar: 0,
+    recoveryTimeFactorVar: 0,
+    deadTimeFactorVar: 0,
+    simulationLengthVar: 0,
+}
+
+const form = document.querySelector(".simulationParameters-form");
+form.addEventListener("submit", handleSubmit);
+function handleSubmit(event) {
+    event.preventDefault()
+    simulator()
+    chartCreatror();
+
+
+    const {
+        elements: { simulationName, populationSize, firstInfected, rFactor, mFactor, recoveryTimeFactor, deadTimeFactor, simulationLength }
+    } = event.currentTarget;
+
+    parameters.simulationNameVar = Number(simulationName.value);
+    parameters.populationSizeVar = Number(populationSize.value);
+    parameters.firstInfectedVar = Number(firstInfected.value);
+    parameters.rFactorVar = Number(rFactor.value);
+    parameters.mFactorVar = Number(mFactor.value);
+    parameters.recoveryTimeFactorVar = Number(recoveryTimeFactor.value);
+    parameters.deadTimeFactorVar = Number(deadTimeFactor.value);
+    parameters.simulationLengthVar = Number(simulationLength.value);
+
+    event.currentTarget.reset();
+}
+
+
+
+let population = [{
     day: 0,
-    infected: firstInfected.value * 1,
-    sensible: populationSize.value * 1 - firstInfected.value * 1,
+    infected: parameters.firstInfectedVar,
+    sensible: parameters.populationSizeVar - parameters.firstInfectedVar,
     dead: 0,
     resistant: 0,
-    livePopulation: populationSize.value * 1,
+    livePopulation: parameters.populationSizeVar,
     newInfected: 0,
     newDeadbyDay: 0,
 }
-    
-    
+
+
 ];
+
+
 
 const datapointsDead = [];
 const datapointsPopulation = [];
@@ -21,85 +60,210 @@ const datapointsLivePopulation = [];
 const datapointsNewDead = [];
 const datapointsNewInfection = [];
 
-for (let i = 1; i <= simulationLength.value; i++) {
 
-    let deadTime = 0;
-    let recoveryTime = 0;
+function simulator() {
+    for (let i = 1; i <= parameters.simulationLengthVar; i++) {
 
-    // Warunki dla mFactor
+        let deadTime = 0;
+        let recoveryTime = 0;
 
-    if (deadTimeFactor.value >= i) {
-        deadtime = 0;
-    } else {
-        deadTime = population[i - deadTimeFactor.value].newInfected;
+        // Warunki dla mFactor
+
+        if (parameters.deadTimeFactorVar >= i) {
+            deadtime = 0;
+        } else {
+            deadTime = population[i - parameters.deadTimeFactorVar].newInfected;
+        };
+
+        let newDead = deadTime * parameters.mFactor;
+
+        // Warunki dla Wyzdrowienia
+
+        if (parameters.recoveryTimeFactorVar >= i) {
+            recoveryTime = 0;
+        } else {
+            recoveryTime = population[i - parameters.recoveryTimeFactorVar].newInfected;
+        };
+
+        let newResistant = recoveryTime;
+
+
+
+        newInfected = population[i - 1].infected * parameters.rFactorVar - population[i - 1].infected;
+
+        if (newInfected > population[i - 1].sensible) {
+            newInfected = population[i - 1].sensible;
+        }
+        else if (newInfected == population[i - 1].sensible) { newInfected = 0 }
+        else {
+
+        };
+
+
+
+        const simulation = {
+            day: i,
+            infected: population[i - 1].infected + population[i - 1].newInfected - newDead - newResistant,
+            sensible: population[i - 1].sensible - newInfected - newDead - newResistant,
+            dead: population[i - 1].dead + newDead,
+            resistant: population[i - 1].resistant + newResistant - newDead,
+            livePopulation: population[i - 1].livePopulation - newDead,
+            newInfected: newInfected,
+            newDeadbyDay: newDead,
+        };
+        if (simulation.sensible <= 0) {
+            simulation.sensible = 0;
+        };
+        if (simulation.infected <= 0) {
+            simulation.infected = 0;
+        }; if (simulation.resistant <= 0) {
+            simulation.resistant = 0;
+        };
+        population.push(simulation);
+        console.log(simulation);
+
+        datapointsDead.push(Math.round(simulation.dead));
+        datapointsPopulation.push(Math.round(simulation.population));
+        datapointsLivePopulation.push(Math.round(simulation.livePopulation));
+        datapointsInfected.push(Math.round(simulation.infected));
+        datapointsSensible.push(Math.round(simulation.sensible));
+        datapointsResistant.push(Math.round(simulation.resistant));
+        datapointsNewDead.push(Math.round(simulation.newDeadbyDay));
+        datapointsNewInfection.push(Math.round(simulation.newInfected));
+
+        console.log(simulation)
+
     };
-
-    let newDead = deadTime * mFactor.value;
-
-    // Warunki dla Wyzdrowienia
-
-    if (recoveryTimeFactor.value >= i) {
-        recoveryTime = 0;
-    } else {
-        recoveryTime = population[i - recoveryTimeFactor.value].newInfected;
-    };
-
-    let newResistant = recoveryTime;
+}
 
 
-
-    newInfected = population[i - 1].infected * rFactor.value - population[i - 1].infected;
-
-    if (newInfected > population[i - 1].sensible) {
-        newInfected = population[i - 1].sensible;
-    }
-    else if (newInfected == population[i - 1].sensible) { newInfected = 0 }
-    else {
-
-    };
-
-
-
-    const simulation = {
-        day: i,
-        infected: population[i - 1].infected + population[i - 1].newInfected - newDead - newResistant,
-        sensible: population[i - 1].sensible - newInfected - newDead - newResistant,
-        dead: population[i - 1].dead + newDead,
-        resistant: population[i - 1].resistant + newResistant - newDead,
-        livePopulation: population[i - 1].livePopulation - newDead,
-        newInfected: newInfected,
-        newDeadbyDay: newDead,
-    };
-    if (simulation.sensible <= 0) {
-        simulation.sensible = 0;
-    };
-    if (simulation.infected <= 0) {
-        simulation.infected = 0;
-    }; if (simulation.resistant <= 0) {
-        simulation.resistant = 0;
-    };
-    population.push(simulation);
-
-    console.log(simulation);
-
-    datapointsDead.push(Math.round(simulation.dead));
-    datapointsPopulation.push(Math.round(simulation.population));
-    datapointsLivePopulation.push(Math.round(simulation.livePopulation));
-    datapointsInfected.push(Math.round(simulation.infected));
-    datapointsSensible.push(Math.round(simulation.sensible));
-    datapointsResistant.push(Math.round(simulation.resistant));
-    datapointsNewDead.push(Math.round(simulation.newDeadbyDay));
-    datapointsNewInfection.push(Math.round(simulation.newInfected));
-
+const CHART_COLORS = {
+    red: 'rgb(255, 99, 132)',
+    orange: 'rgb(255, 159, 64)',
+    yellow: 'rgb(255, 205, 86)',
+    green: 'rgb(75, 192, 192)',
+    blue: 'rgb(54, 162, 235)',
+    purple: 'rgb(153, 102, 255)',
+    grey: 'rgb(201, 203, 207)', black: 'rgb(0, 0, 0)'
 };
 
 
-exports.dataDead = datapointsDead;
-exports.dataLivePopulation = datapointsLivePopulation;
-exports.dataInfected = datapointsInfected;
-exports.dataSensible = datapointsSensible;
-exports.dataResistant = datapointsResistant;
-exports.dataSensible = datapointsSensible;
-exports.dataResistant = datapointsResistant;
-exports.dataNewDead = datapointsNewDead;
-exports.dataNewInfection = datapointsNewInfection;
+const DATA_COUNT = parameters.simulationLengthVar;
+const labels = [];
+for (let i = 0; i < DATA_COUNT; ++i) {
+    labels.push(i.toString());
+};
+
+const data = {
+    labels: labels,
+    datasets: [
+        {
+            label: 'Osoby Zmarłe',
+            data: datapointsDead,
+            borderColor: CHART_COLORS.red,
+            fill: false,
+            cubicInterpolationMode: 'monotone',
+            tension: 0.8,
+            yAxisID: 'y',
+        },
+        {
+            label: 'Populacja',
+            data: datapointsLivePopulation,
+            borderColor: CHART_COLORS.orange,
+            fill: false,
+            cubicInterpolationMode: 'monotone',
+            tension: 0.8,
+            yAxisID: 'y',
+        },
+        {
+            label: 'Zainfekowani',
+            data: datapointsInfected,
+            borderColor: CHART_COLORS.yellow,
+            fill: false,
+            cubicInterpolationMode: 'monotone',
+            tension: 0.8,
+            yAxisID: 'y',
+        },
+        {
+            label: 'Osoby Podatne',
+            data: datapointsSensible,
+            borderColor: CHART_COLORS.blue,
+            fill: false,
+            cubicInterpolationMode: 'monotone',
+            tension: 0.8,
+            yAxisID: 'y',
+        },
+        {
+            label: 'Osoby Odporne',
+            data: datapointsResistant,
+            borderColor: CHART_COLORS.green,
+            fill: false,
+            cubicInterpolationMode: 'monotone',
+            tension: 0.8,
+            yAxisID: 'y',
+        },
+        {
+            label: 'Nowi Zmarli',
+            data: datapointsNewDead,
+            borderColor: CHART_COLORS.black,
+            fill: false,
+            cubicInterpolationMode: 'monotone',
+            tension: 0.8,
+            yAxisID: 'y1',
+        },
+        {
+            label: 'Nowe Zakarzenia',
+            data: datapointsNewInfection,
+            borderColor: CHART_COLORS.grey,
+            fill: false,
+            cubicInterpolationMode: 'monotone',
+            tension: 0.8,
+            yAxisID: 'y1',
+        }
+    ]
+};
+
+const config = {
+    type: 'line',
+    data: data,
+    options: {
+        responsive: true,
+        interaction: {
+            mode: 'index',
+            intersect: false,
+        },
+        stacked: false,
+        plugins: {
+            title: {
+                display: true,
+                text: 'Sybulacja Zagłady Ludzkości'
+            }
+        },
+        scales: {
+            y: {
+                type: 'linear',
+                display: true,
+                position: 'left',
+            },
+            y1: {
+                type: 'linear',
+                display: true,
+                position: 'right',
+
+                // grid line settings
+                grid: {
+                    drawOnChartArea: false, // only want the grid lines for one axis to show up
+                },
+            },
+        }
+    },
+};
+
+
+function chartCreatror() {
+    const myChart = new Chart(
+        document.getElementById('myChart'),
+        config
+    );
+}
+
